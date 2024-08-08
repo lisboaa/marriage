152b042315eb43788651370b2bc905fc2cbf0d19
Douglas Lisboa <douglasfernandolisboaa@gmail.com>
2024-08-07 21:50:04 -0300

    Modificado nome localização.

--- src/app/local-evento/page.tsx
+++ src/app/local-evento/page.tsx
@@ -72,7 +72,7 @@ export default function ListaPresente() {
         </div>
         <div className='flex flex-col'>
           <h1 className='text-4xl flex justify-center'>Local Evento</h1>
-          <span className='w-2/3 text-xl mt-28 text-wrap font-sans font-medium pl-5'>Saída para Campo Mourão. Rodovia 317, km 114, Maringá 87015090</span>
+          <span className='w-2/3 text-xl mt-28 text-wrap font-sans font-medium pl-5'>Eden Garden Saída para Campo Mourão. Rodovia 317, km 114, Maringá 87015090</span>
           <a className='text-cyan-600 text-xl mt-5 text-wrap flex justify-center pt-10' target='_blank' href='https://maps.app.goo.gl/amvCThSAUin7Mxwb9?g_st=aw'>Localização</a>
         </div>
           
