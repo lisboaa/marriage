53d9bbd04f59e741cd27f31b9356c33e3dc01824
Douglas Lisboa <douglasfernandolisboaa@gmail.com>
2024-08-19 09:21:00 -0300

    Modificado texto nossa historia.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,12 +101,7 @@ export default function Home() {
             </CarouselContent>
           </Carousel> */}
           <p className="px-5 text-lg">
-            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
-            versions of Lorem Ipsum.
+          E após 6 anos de muitas alegrias e descobertas, com a benção de Deus, nossa história de amor terá um novo capítulo, e é  claro que você faz parte disso e com imenso prazer te convidamos a vivenciar esse momento conosco, te aguardamos para fazer do nosso  dia 15, ainda mais especial.
           </p>
         </div>
       </div>
